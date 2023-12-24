0269fa6bcfbf3ae8488658ea6a8c4d524dd42729
stablechoi <stablechoi@proton.me>
2023-12-24 23:33:53 +0900

    metadata

--- src/Album.sol
+++ src/Album.sol
@@ -4,14 +4,27 @@ pragma solidity 0.8.21;
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract Album is ERC1155 {
-    constructor() ERC1155("") {}
+    constructor(string memory name_, string memory symbol_) ERC1155("") {
+        _name = name_;
+        _symbol = symbol_;
+    }
 
     /**
      * @dev TODO
-     * - metadata
      * - mint
      * - CUSD
      * - ERC20Permit
      * - Gas Relay
      */
+
+    string private _name;
+    string private _symbol;
+
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
 }
