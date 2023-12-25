1483181e9d3c0809409628774a28655f76e1677d
stablechoi <stablechoi@proton.me>
2023-12-25 14:11:30 +0900

    erc1155 => erc721a

--- src/Album.sol
+++ src/Album.sol
@@ -1,30 +1,4 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
-import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-
-contract Album is ERC1155 {
-    constructor(string memory name_, string memory symbol_) ERC1155("") {
-        _name = name_;
-        _symbol = symbol_;
-    }
-
-    /**
-     * @dev TODO
-     * - mint
-     * - CUSD
-     * - ERC20Permit
-     * - Gas Relay
-     */
-
-    string private _name;
-    string private _symbol;
-
-    function name() public view returns (string memory) {
-        return _name;
-    }
-
-    function symbol() public view returns (string memory) {
-        return _symbol;
-    }
-}
+import {ERC721A} from "erc721a/contracts/ERC721A.sol";
