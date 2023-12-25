58ef40c111fd2a333c3cf0379bb60842d400bb83
stablechoi <stablechoi@proton.me>
2023-12-25 21:03:35 +0900

    AlbumFactory

---
+++ src/AlbumFactory.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
+import {IAlbum} from "./interfaces/IAlbum.sol";
+
+contract Pactory {
+    event ReleasedAlbum(address indexed);
+
+    address immutable impl;
+
+    constructor(address _impl) {
+        impl = _impl;
+    }
+
+    function clone(string memory name) public returns (address) {}
+}
