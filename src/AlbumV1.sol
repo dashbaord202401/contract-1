044a185feb530d2ccaeebde5e408728b54e6b05b
stablechoi <stablechoi@proton.me>
2023-12-26 09:54:26 +0900

    mint method

--- src/AlbumV1.sol
+++ src/AlbumV1.sol
@@ -5,7 +5,7 @@ import {ERC721AUpgradeable} from "erc721a-upgradeable/contracts/ERC721AUpgradeab
 import {IVRC25} from "./interfaces/IVRC25.sol";
 
 contract Album is ERC721AUpgradeable {
-    IVRC25 public CUSD = IVRC25(0xb3008E7156Ae2312b49B5200C3E1C3e80E529feb);
+    address public immutable CUSD = 0xb3008E7156Ae2312b49B5200C3E1C3e80E529feb;
     string private _baseTokenURI;
     uint256 public ALBUM_PRICE;
 
@@ -22,5 +22,10 @@ contract Album is ERC721AUpgradeable {
 
     function _baseURI() internal view override returns (string memory) {
         return _baseTokenURI;
+    }
+
+    function mint(uint256 quantity) public {
+        IVRC25(CUSD).transfer(address(this), ALBUM_PRICE * quantity);
+        _safeMint(msg.sender, quantity);
     }
 }
