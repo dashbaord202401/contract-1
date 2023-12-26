4086d1a5432e81158ceb17fd7c08306662aeb693
stablechoi <stablechoi@proton.me>
2023-12-26 17:40:09 +0900

    final commit

--- src/Fanding.sol
+++ src/Fanding.sol
@@ -3,17 +3,25 @@ pragma solidity 0.8.21;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Fanding is Ownable, ERC1155 {
     // Metadata storage
     string private _name = "Album";
     string private _symbol = "ALBUM";
 
+    // Token storage
+    IERC20 public CUSD;
+
+    // Price storage
+    uint256 public price = 10;
+
     // Consturctor
     constructor() Ownable(msg.sender) ERC1155("") {}
 
     // Buy & Withdraw function
     function buyAlbum(uint256 id, uint256 quantity) public {
+        CUSD.transfer(owner(), price);
         _mint(msg.sender, id, quantity, "");
     }
 
@@ -29,5 +37,15 @@ contract Fanding is Ownable, ERC1155 {
     // Setting function
     function setTokenUri(string calldata newUri) external onlyOwner {
         _setURI(newUri);
+    }
+
+    // Set token address
+    function setTokenAddr(address addr) public onlyOwner {
+        CUSD = IERC20(addr);
+    }
+
+    // Set price
+    function setPrice(uint256 newPrice) public onlyOwner {
+        price = newPrice;
     }
 }
--- src/Mock20.sol
+++ src/Mock20.sol
@@ -2,3 +2,11 @@
 pragma solidity 0.8.21;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Mock20 is ERC20 {
+    constructor() ERC20("Mock", "MOCK") {}
+
+    function faucet(uint256 value) public {
+        _mint(msg.sender, value);
+    }
+}
