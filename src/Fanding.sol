6b9d57d8bbaf0407b64100592a754163d28d048d
stablechoi <stablechoi@proton.me>
2023-12-26 15:46:01 +0900

    wip

--- src/Fanding.sol
+++ src/Fanding.sol
@@ -5,10 +5,25 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract Fanding is Ownable, ERC1155 {
-    constructor() Ownable(msg.sender) ERC1155("") {}
-
     string private _name = "Album";
     string private _symbol = "ALBUM";
+
+    uint256 public constant ALBUM = 1;
+
+    constructor() Ownable(msg.sender) ERC1155("") {}
+
+    function buyAlbum(uint256 quantity) public {
+        _mint(msg.sender, ALBUM, quantity, "");
+    }
+
+    function withdraw() external onlyOwner {
+        (bool success, ) = msg.sender.call{value: address(this).balance}("");
+        require(success, "Transfer failed.");
+    }
+
+    function setTokenUri(string calldata newUri) external onlyOwner {
+        _setURI(newUri);
+    }
 
     function name() public view returns (string memory) {
         return _name;
