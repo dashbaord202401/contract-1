d45db62edaec3e73aa6f482d7d98c3fc3ff7ff2b
stablechoi <stablechoi@proton.me>
2023-12-26 15:39:22 +0900

    ERC1155

--- src/Fanding.sol
+++ src/Fanding.sol
@@ -2,43 +2,27 @@
 pragma solidity 0.8.21;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {ERC721A} from "erc721a/contracts/ERC721A.sol";
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-contract Fanding is Ownable, ERC721A {
-    uint256 public price;
-    string private _baseTokenURI;
+contract Fanding is Ownable, ERC1155 {
+    constructor() Ownable(msg.sender) ERC1155("") {}
 
-    constructor() Ownable(msg.sender) ERC721A("Album", "ALBUM") {}
+    string private _name = "Album";
+    string private _symbol = "ALBUM";
 
-    function mint(uint256 quantity) external payable {
-        pay(quantity);
-        _safeMint(msg.sender, quantity);
+    function name() public view returns (string memory) {
+        return _name;
     }
 
-    function pay(uint256 quantity) private {
-        (bool success, ) = owner().call{value: price * quantity}("");
-        require(success, "Failed to send Ether");
+    function symbol() public view returns (string memory) {
+        return _symbol;
     }
 
-    function setPrice(uint256 _price) external onlyOwner {
-        price = _price;
-    }
-
-    function _baseURI() internal view virtual override returns (string memory) {
-        return _baseTokenURI;
-    }
-
-    function setBaseURI(string calldata baseURI) external onlyOwner {
-        _baseTokenURI = baseURI;
-    }
-
-    function numberMinted(address owner) public view returns (uint256) {
-        return _numberMinted(owner);
-    }
-
-    function getOwnershipData(
-        uint256 tokenId
-    ) external view returns (TokenOwnership memory) {
-        return _ownershipOf(tokenId);
+    function setNameAndSymbol(
+        string calldata _newName,
+        string calldata _newSymbol
+    ) external onlyOwner {
+        _name = _newName;
+        _symbol = _newSymbol;
     }
 }
