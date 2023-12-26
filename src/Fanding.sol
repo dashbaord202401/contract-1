f032a3dfa0bd650a82f9b668249eeb8728ab3409
stablechoi <stablechoi@proton.me>
2023-12-26 15:58:27 +0900

    FandingV1

--- src/Fanding.sol
+++ src/Fanding.sol
@@ -7,6 +7,7 @@ import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 contract Fanding is Ownable, ERC1155 {
     string private _name = "Album";
     string private _symbol = "ALBUM";
+    uint256 public price;
 
     uint256 public constant ALBUM = 1;
 
@@ -14,11 +15,17 @@ contract Fanding is Ownable, ERC1155 {
 
     function buyAlbum(uint256 quantity) public {
         _mint(msg.sender, ALBUM, quantity, "");
+        (bool success, ) = owner().call{value: price * quantity}("");
+        require(success, "Failed to send Ether");
     }
 
     function withdraw() external onlyOwner {
         (bool success, ) = msg.sender.call{value: address(this).balance}("");
         require(success, "Transfer failed.");
+    }
+
+    function setPrice(uint256 newPrice) external onlyOwner {
+        price = newPrice;
     }
 
     function setTokenUri(string calldata newUri) external onlyOwner {
