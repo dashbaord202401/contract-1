91a624c32417a59fb3b6a5766e7efc3d2da5e57d
stablechoi <stablechoi@proton.me>
2023-12-26 14:45:28 +0900

    price method

--- src/Fanding.sol
+++ src/Fanding.sol
@@ -5,6 +5,7 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC721A} from "erc721a/contracts/ERC721A.sol";
 
 contract Fanding is Ownable, ERC721A {
+    uint256 private _price;
     string private _baseTokenURI;
 
     constructor() Ownable(msg.sender) ERC721A("Album", "ALBUM") {}
@@ -13,11 +14,29 @@ contract Fanding is Ownable, ERC721A {
         _mint(msg.sender, quantity);
     }
 
+    function price() internal view returns (uint256 memory) {
+        return _price;
+    }
+
+    function setPrice(uint256 calldata price) external onlyOwner {
+        _price = price;
+    }
+
     function _baseURI() internal view virtual override returns (string memory) {
         return _baseTokenURI;
     }
 
     function setBaseURI(string calldata baseURI) external onlyOwner {
         _baseTokenURI = baseURI;
+    }
+
+    function numberMinted(address owner) public view returns (uint256) {
+        return _numberMinted(owner);
+    }
+
+    function getOwnershipData(
+        uint256 tokenId
+    ) external view returns (TokenOwnership memory) {
+        return ownershipOf(tokenId);
     }
 }
