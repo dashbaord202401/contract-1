3284c7afb123ccf1c0ec01d7b56274ed7c5abe08
stablechoi <stablechoi@proton.me>
2023-12-26 14:31:27 +0900

    metadata method

--- src/Fanding.sol
+++ src/Fanding.sol
@@ -1,14 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC721A} from "erc721a/contracts/ERC721A.sol";
 
-contract Fanding is ERC721A {
-    address public immutable CUSD = 0xb3008E7156Ae2312b49B5200C3E1C3e80E529feb;
+contract Fanding is Ownable, ERC721A {
+    string private _baseTokenURI;
 
-    constructor() ERC721A("Album", "ALBUM") {}
+    constructor() Ownable(msg.sender) ERC721A("Album", "ALBUM") {}
 
     function mint(uint256 quantity) external payable {
         _mint(msg.sender, quantity);
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function setBaseURI(string calldata baseURI) external onlyOwner {
+        _baseTokenURI = baseURI;
     }
 }
