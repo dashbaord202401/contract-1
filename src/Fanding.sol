183d190a994579b8b5f36cca5fbb6a2dc6b7df26
stablechoi <stablechoi@proton.me>
2023-12-26 15:58:49 +0900

    FandingV1

--- src/Fanding.sol
+++ src/Fanding.sol
@@ -5,14 +5,20 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract Fanding is Ownable, ERC1155 {
+    // Metadata storage
     string private _name = "Album";
     string private _symbol = "ALBUM";
+
+    // Price storage
     uint256 public price;
 
+    // Defaut tokenId is `1`
     uint256 public constant ALBUM = 1;
 
+    // Consturctor
     constructor() Ownable(msg.sender) ERC1155("") {}
 
+    // Buy & Withdraw function
     function buyAlbum(uint256 quantity) public {
         _mint(msg.sender, ALBUM, quantity, "");
         (bool success, ) = owner().call{value: price * quantity}("");
@@ -24,20 +30,22 @@ contract Fanding is Ownable, ERC1155 {
         require(success, "Transfer failed.");
     }
 
-    function setPrice(uint256 newPrice) external onlyOwner {
-        price = newPrice;
-    }
-
-    function setTokenUri(string calldata newUri) external onlyOwner {
-        _setURI(newUri);
-    }
-
+    // Metadata function
     function name() public view returns (string memory) {
         return _name;
     }
 
     function symbol() public view returns (string memory) {
         return _symbol;
+    }
+
+    // Setting function
+    function setPrice(uint256 newPrice) external onlyOwner {
+        price = newPrice;
+    }
+
+    function setTokenUri(string calldata newUri) external onlyOwner {
+        _setURI(newUri);
     }
 
     function setNameAndSymbol(
