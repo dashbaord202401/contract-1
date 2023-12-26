20bb3aaa6f83c6355bb57f2330ced04059741d18
stablechoi <stablechoi@proton.me>
2023-12-26 17:15:00 +0900

    fin

--- src/Fanding.sol
+++ src/Fanding.sol
@@ -9,28 +9,12 @@ contract Fanding is Ownable, ERC1155 {
     string private _name = "Album";
     string private _symbol = "ALBUM";
 
-    // Price storage
-    uint256 public ALBUM_PRICE = 1 ether;
-
     // Consturctor
     constructor() Ownable(msg.sender) ERC1155("") {}
 
     // Buy & Withdraw function
-    function buyAlbum(uint256 id, uint256 quantity) public payable {
+    function buyAlbum(uint256 id, uint256 quantity) public {
         _mint(msg.sender, id, quantity, "");
-        refundIfOver(ALBUM_PRICE * quantity);
-    }
-
-    function refundIfOver(uint256 price) private {
-        require(msg.value >= price, "Need to send more ETH.");
-        if (msg.value > price) {
-            payable(msg.sender).transfer(msg.value - price);
-        }
-    }
-
-    function withdraw() external onlyOwner {
-        (bool success, ) = msg.sender.call{value: address(this).balance}("");
-        require(success, "Transfer failed.");
     }
 
     // Metadata function
